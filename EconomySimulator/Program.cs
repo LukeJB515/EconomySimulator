139c0466e708297d0d6a2b07dc031e45b958412e
LukeJB515 <lukeest1984@gmail.com>
2026-01-17 17:28:29 -0600

    first commit just a few console writelines

---
+++ EconomySimulator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace EconomySimulator
+{
+    public class Program()
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to the game.");
+            Console.WriteLine("It is luck based.");
+            Console.WriteLine("So if you win, don't give any credit to your skill.");
+            Console.WriteLine("Nobody has real skills in Economy Simulator.");
+        }
+    }
+}
